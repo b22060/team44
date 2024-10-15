04ce485530d4573d1bf0eed2bef00803210dd849
b22060 <e1b22060@oit.ac.jp>
2024-10-15 13:54:48 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2535/dai4_team4/team44/Team44Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2535.dai4_team4.team44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team44Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team44Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2535/dai4_team4/team44/Team44ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2535.dai4_team4.team44;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team44ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
