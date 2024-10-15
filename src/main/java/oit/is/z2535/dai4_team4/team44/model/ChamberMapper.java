c248e9ef4262dc70bc22f4b938369adee482108a
Yura-Mp <e1b22079@oit.ac.jp>
2024-10-15 15:00:16 +0900

    Sample4-2完了

---
+++ src/main/java/oit/is/z2535/dai4_team4/team44/model/ChamberMapper.java
@@ -0,0 +1,16 @@
+package oit.is.z2535.dai4_team4.team44.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface ChamberMapper {
+
+  @Select("SELECT id,userName,chamberName from chamber where id = #{id}")
+  Chamber selectById(int id);
+
+}
